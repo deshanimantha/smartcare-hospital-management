36e7eaf94453895b6104d1b7bcc6857b046a26f8
deshanimantha <imantha134@gmail.com>
2026-08-22 22:22:40 +0530

    Initial SmartCare hospital management backend

--- hospital-management/src/main/java/com/smartcare/hospital_management/service/PatientService.java
+++ hospital-management/src/main/java/com/smartcare/hospital_management/service/PatientService.java
@@ -13,10 +13,12 @@ public class PatientService {
     private final PatientRepository patientRepository;
 
     public PatientService(PatientRepository patientRepository) {
+
         this.patientRepository = patientRepository;
     }
 
     public Patient registerPatient(Patient patient) {
+
         return patientRepository.save(patient);
     }
 
